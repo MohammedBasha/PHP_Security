b1463cd50616f0fbb6b02f5c71b22293e23bd6f5
MohammedBasha <me@mohammedbasha.com>
2021-07-28 23:53:41 +0200

    SQL Injection

--- index.php
+++ index.php
@@ -2,19 +2,33 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP security tips | XSS => Cross Site Scripting</title>
+    <title>PHP security tips | SQL Injection</title>
 </head>
 <body>
 <?php
+// http://www.securityidiots.com/Web-Pentest/SQL-Injection/Basic-Union-Based-SQL-Injection.html
 
-// https://www.php.net/manual/en/filter.filters.sanitize.php
-// https://cheatsheetseries.owasp.org/cheatsheets/Cross_Site_Scripting_Prevention_Cheat_Sheet.html
+require 'connect.php';
 
- if (isset($_GET['search'])) {
-     // index.php?search=ok<script>alert('ok')</script>
-     echo 'Keyword ' . filter_var($_GET['search'], FILTER_SANITIZE_STRING);
-     // after running will output: Keyword okalert('ok')
- }
+if (isset($_GET['id']) && !empty($_GET['id'])) {
+    $userid = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+    $stmt = $con->prepare('SELECT * FROM members WHERE id = ?');
+    $stmt->execute([$userid]);
+    $count = $stmt->rowCount();
+    if ($count > 0) {
+        while ($row = $stmt->fetch()) {
+            $id = $row['id'];
+            $name = $row['name'];
+        }
+        echo $id . ' ' . $name;
+    } else {
+        echo 'There is no id like that';
+    }
+
+} else {
+    echo 'Please we need a valid id';
+}
+
 ?>
 </body>
 </html>
