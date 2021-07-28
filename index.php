087cee3526124f33ec0096ed4e31eff74d8eb46d
MohammedBasha <me@mohammedbasha.com>
2021-07-29 00:59:25 +0200

    Disable Directory Listing

--- index.php
+++ index.php
@@ -2,24 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP security tips | Password Hash</title>
+    <title>PHP security tips | Disable Directory Listing</title>
 </head>
 <body>
 <?php
-// https://www.php.net/manual/en/function.password-hash.php
-// https://www.php.net/manual/en/function.password-verify.php
 
-$password = 'mohammed123';
-$theGoodHashedPassword = password_hash($password, PASSWORD_DEFAULT);
-echo $theGoodHashedPassword . '<br>';
-$hashedPassword = md5($password);
-echo $hashedPassword . '<br>';
 
-if (password_verify($password, $theGoodHashedPassword)) {
-    echo 'The Good one';
-} else {
-    echo 'The bad one';
-}
 ?>
 </body>
 </html>
