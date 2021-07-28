35ac76061e578f5d85c2a34c27f70e973aef090f
MohammedBasha <me@mohammedbasha.com>
2021-07-29 01:07:07 +0200

    Header Location Redirect the right way

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP security tips | Disable Directory Listing</title>
+    <title>PHP security tips | Header Location Redirect the right way</title>
 </head>
 <body>
 <?php
 
+$id = 0;
+
+if ($id !== 1) {
+    header('Location: test.php');
+    exit; // we should exit the script after redirecting
+}
+
+echo 'This info will be shown for admins only';
 
 ?>
 </body>
