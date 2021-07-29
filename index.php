42ba8c16a253795321b3b77f4e6f72222ad8c248
MohammedBasha <me@mohammedbasha.com>
2021-07-29 02:46:43 +0200

    Protect directory with IP

--- index.php
+++ index.php
@@ -2,19 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP security tips | Header Location Redirect the right way</title>
+    <title>PHP security tips | Protect directory with IP</title>
 </head>
 <body>
 <?php
-
-$id = 0;
-
-if ($id !== 1) {
-    header('Location: test.php');
-    exit; // we should exit the script after redirecting
-}
-
-echo 'This info will be shown for admins only';
 
 ?>
 </body>
