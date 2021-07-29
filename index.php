749cbf3453d30b62d9a07f1f6a90379eaf9f0d04
MohammedBasha <me@mohammedbasha.com>
2021-07-29 02:55:52 +0200

    Prevent execute specific files

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>PHP security tips | Protect directory with IP</title>
+    <title>PHP security tips | Prevent execute specific files</title>
 </head>
 <body>
 <?php
+// https://www.askapache.com/htaccess/using-filesmatch-and-files-in-htaccess/
+
 
 ?>
 </body>
